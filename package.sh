a1aae1bdd35250f1d94bfb8a1f5a70ea69658283
Benjamin Gehrels <github@gehrels>
2015-09-27 18:28:07 +0200

    extended the script to support building release from git tags

--- package.sh
+++ package.sh
@@ -3,20 +3,30 @@
 # Get the original sources from Douglas Crockfords GitHub repo
 git clone https://github.com/douglascrockford/JSON-java.git original
 
+echo ""
+echo "The following tags exist in the repository:"
+cd original
+git tag -l 
+
+echo "Please enter the tag name to be released"
+read tagName
+
+git checkout tags/${tagName}
+cd ..
+
 # repackage them into maven standard layout
 mkdir -p dist/src/main/java/org/json
-rm original/README
 mv original/* dist/src/main/java/org/json/
+rm dist/src/main/java/org/json/README
+rm -Rf original
 
 # Add a pom.xml with the current date as version
 cp pom.xml dist/
-sed -i "s/%%VERSION%%/`date +%Y%m%d`/g" dist/pom.xml
+sed -i "s/%%VERSION%%/${tagName}/g" dist/pom.xml
 
-# Build and release it
-cd dist
-mvn deploy -Psonatype-oss-release
-cd ..
+ech ""
+echo "Please run mvn deploy in the dist folder"
+echo "use a shell with a working gpg2 command. Hit <enter> afterwards."
+read succes
 
-# and clean everything up again
-rm -Rf original
 rm -Rf dist

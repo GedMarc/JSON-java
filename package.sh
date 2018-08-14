5a81e9476eb1d5ec92e4f81301657e074c5bdaa9
Benjamin Gehrels <git@gehrels.info>
2018-08-14 02:59:29 +0200

    Fix paths on windows 10

--- package.sh
+++ package.sh
@@ -8,7 +8,7 @@ git clone https://github.com/stleary/JSON-java.git $SRC_FOLDER
 
 echo ""
 echo "The following tags exist in the repository:"
-cd $SRC_FOLDER
+cd "$SRC_FOLDER"
 git tag -l
 
 echo ""
@@ -18,24 +18,24 @@ read tagName
 git checkout tags/${tagName}
 
 # Checkout the tags from the unit test repo
-cd $WORKING_DIR
+cd "$WORKING_DIR"
 git clone https://github.com/stleary/JSON-Java-unit-test.git tests
 cd tests
 git checkout tags/${tagName}
 
 # And copy them into the build folder structure
-cd $WORKING_DIR
+cd "$WORKING_DIR"
 mkdir -p dist/src/test
 mv tests/src/test/* dist/src/test/
 rm -Rf tests
 
 # Remove the README file from the package structure
-cd $SRC_FOLDER
-rm README
+cd "$SRC_FOLDER"
+rm README.md
 
 # Add a pom.xml
 # Since the tests are written in java 7 and the code itself has to be built with java 6, we du two runs here
-cd $WORKING_DIR
+cd "$WORKING_DIR"
 cp pom.xml dist/
 sed -i "s/%%VERSION%%/${tagName}/g" dist/pom.xml
 sed -i "s/%%JAVAVERSION%%/1.7/g" dist/pom.xml
@@ -45,7 +45,7 @@ echo "Please run mvn verify -Dgpg.passphrase=thepassphrase in the dist folder"
 read success
 
 # Add a pom.xml with the current date as version
-cd $WORKING_DIR
+cd "$WORKING_DIR"
 rm dist/pom.xml
 cp pom.xml dist/
 sed -i "s/%%VERSION%%/${tagName}/g" dist/pom.xml
